2ef46ec3f2066c9b1574cbc01515fd696c8859ba
Gwladys Gely <gwladysgely@gmail.com>
2026-05-21 09:57:41 -0400

    changement nom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Noctura Sleep Aid - Sommeil Serein',
+  title: 'Noctura Sleep Aid - Fè Moun Dòmi',
   description: 'Application d’aide à la gestion de l’insomnie basée sur 4 piliers.',
 };
 
